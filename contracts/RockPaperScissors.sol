5b02944c5ca309343bca307abe6643f25efc2627
Ayman NAIT CHERIF <ayman.naitcherif@adria-bt.com>
2022-04-26 23:43:29 +0000

    feature : add option for the second player to play its role

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -11,6 +11,7 @@ contract RockPaperScissors {
         string secoundPlayerChoice;
         address winner;
         bool isFinished;
+        bool exists;
     }
 
     mapping(address => Game) public games;
@@ -24,6 +25,21 @@ contract RockPaperScissors {
             msg.sender == owner,
             "Only the owner is allowed to do this operation"
         );
+        _;
+    }
+
+    modifier validChoice(uint256 choiceId) {
+        require(choiceId < options.length, "Invalid choice Id !");
+        _;
+    }
+
+    modifier gameExists(address gameId) {
+        require(games[gameId].exists, "Invalid gameId !");
+        _;
+    }
+
+    modifier gameFinished(address gameId) {
+        require(games[gameId].isFinished == false, "This game has finished !");
         _;
     }
 
@@ -49,15 +65,38 @@ contract RockPaperScissors {
         options = _options;
     }
 
-    function initGame(string calldata choice) external returns (address) {
+    function initGame(uint256 choiceId)
+        external
+        validChoice(choiceId)
+        returns (address)
+    {
         Game memory game;
         game.firstPlayer = msg.sender;
-        game.firstPlayerChoice = choice;
-        game.isFinished = false;
+        game.firstPlayerChoice = options[choiceId];
+        game.exists = true;
 
         games[msg.sender] = game;
         console.log(msg.sender);
 
         return msg.sender;
+    }
+
+    function play(address gameId, uint256 choiceId)
+        external
+        validChoice(choiceId)
+        gameExists(gameId)
+        gameFinished(gameId)
+        returns (Game memory)
+    {
+        require(
+            msg.sender != games[gameId].firstPlayer,
+            "You've already played your role !"
+        );
+
+        games[gameId].secoundPlayer = msg.sender;
+        games[gameId].secoundPlayerChoice = options[choiceId];
+        games[gameId].isFinished = true;
+
+        return games[gameId];
     }
 }
